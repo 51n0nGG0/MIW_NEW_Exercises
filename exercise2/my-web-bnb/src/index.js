41afc979fedb1b801e96872cc9ea78ecde529c47
Eloy Alfredo Schmidt Rodríguez <UO271588@uniovi.es>
2024-12-16 20:31:47 +0100

    feat:Actualizar index.js

--- exercise2/my-web-bnb/src/index.js
+++ exercise2/my-web-bnb/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import detectEthereumProvider from "@metamask/detect-provider";
+import { Contract, ethers } from "ethers";
+import myContractManifest from "./contracts/MyContract.json";
+import { useState, useEffect, useRef } from 'react';
 
 function App(){
-  return (
-      <div>
-          <h1>Hello World</h1>
-      </div>
-  )
+  let configureBlockchain = async () => {
+    try {
+      let provider = await detectEthereumProvider();
+      if (provider) {
+        await provider.request({ method: 'eth_requestAccounts' });
+        const networkId = await provider.request({ method: 'net_version' })
+
+        provider = new ethers.providers.Web3Provider(provider);
+        const signer = provider.getSigner();
+
+      }
+    } catch (error) { }
+  }
+
+  const tempContract = new Contract(
+    'COPIAR AQUI LA ADDRESS DE NUESTRO CONTRATO – como String',
+    myContractManifest.abi,
+    signer
+  );
+
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
